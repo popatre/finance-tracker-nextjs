18734a8cae82449090503edb1e53f04b2d627b1e
popatre <76402245+popatre@users.noreply.github.com>
2022-05-26 21:47:03 +0100

    possible fix for image

--- components/Metatags.js
+++ components/Metatags.js
@@ -8,11 +8,11 @@ export default function Metatags({ title, description, image }) {
             <meta name="twitter:site" content="@popatre" />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} />
+            <meta name="twitter:image" content={`${image}`} />
 
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
+            <meta property="og:image" content={`${image}`} />
 
             <meta property="title" content={title} />
             <meta property="description" content={description} />
