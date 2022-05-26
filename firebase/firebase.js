d44ac7224b8f73372a158ab6725542b662283bd7
popatre <76402245+popatre@users.noreply.github.com>
2022-05-26 21:21:54 +0100

    add db setting to ignore undefined

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,6 +19,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+initializeFirestore(app, {
+    ignoreUndefinedProperties: true,
+});
+
 const db = getFirestore(app);
 const auth = getAuth();
 
