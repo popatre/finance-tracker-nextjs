b3e1801d1c1c3eba433c52c038a53d6e958a0dae
popatre <76402245+popatre@users.noreply.github.com>
2023-01-26 16:55:14 +0000

    update 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,9 @@
+import styles from "../styles/Error404.module.css";
+
 export default function Page404() {
-    return <h1>You gone wrong there</h1>;
+    return (
+        <div className={styles.container}>
+            <h1>404 - Page Not Found</h1>
+        </div>
+    );
 }
