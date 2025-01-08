c7baf7f813f053c0bea64d25ad27d93287be1e35
popatre <76402245+popatre@users.noreply.github.com>
2025-01-08 22:36:22 +0000

    update year selection to include 2025 plus future proof to dynamically update

--- components/YearDropDown.jsx
+++ components/YearDropDown.jsx
@@ -12,6 +12,22 @@ function YearDropDown({ setYear, year }) {
         const { value } = e.target;
         setYear(value);
     };
+
+    const currentYear = new Date().getFullYear();
+    const maxYear = currentYear + 1;
+
+    function generateYearOptions() {
+        const options = [];
+        for (let year = currentYear; year <= maxYear; year++) {
+            options.push(
+                <option key={year} value={year}>
+                    {year}
+                </option>
+            );
+        }
+        return options;
+    }
+
     return (
         <form className="dropdown__form">
             <label htmlFor="years">Choose a year:</label>
@@ -21,6 +37,7 @@ function YearDropDown({ setYear, year }) {
                 <option value="2022">2022</option>
                 <option value="2023">2023</option>
                 <option value="2024">2024</option>
+                {generateYearOptions()}
             </select>
         </form>
     );
